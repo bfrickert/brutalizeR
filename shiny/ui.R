bc428f1d30950c44c8cb7cfbf3cb71910ea76a50
Brian Frickert <bfrickert@yahoo.com>
2016-02-18 22:02:00 -0500

    update intro to more better, fluidly explain premise

--- shiny/ui.R
+++ shiny/ui.R
@@ -9,10 +9,13 @@ shinyUI(fluidPage(theme='bootstrap.css',
       br(),br(),br(),br(),br(),br(),br(),br(),
       HTML("<h2 class='XXIIBlackmetalWarrior'>A Satanic Study of Time Series Variance</h2>"),
       column(12, HTML("<h5>Is there a way to quantify the <strong>brutality</strong>
-            of a black metal song?<br /><br />Probably not. However, if there were a way, it would 
-            probably be a measurement of what ratio of the song, when blended with <i>Vacation</i> 
+            of a black metal song?<br /><br />Chances are there isn't. Nevertheless, if there 
+            were a way, it would 
+            probably be a measurement of what ratio of an individual black metal song, 
+            when blended with <i>Vacation</i> 
             by the 
-           <strong>Go-Go's</strong> statistically turns <i>Vacation</i> into a black metal song.</h5>")),
+           <strong>Go-Go's</strong> statistically turns <i>Vacation</i> into a black metal 
+                      song.</h5>")),
       tableOutput('brutality.coefs')
   )
   )
