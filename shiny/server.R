5eb422ed8a2178f23a23b51de84a70296061e63b
Brian Frickert <bfrickert@yahoo.com>
2016-02-18 21:20:16 -0500

    added image and table to shiny app

--- shiny/server.R
+++ shiny/server.R
@@ -3,6 +3,23 @@ source('helper.R')
 
 shinyServer(function(input, output) {
  
+  output$banner <- renderImage({
+    list(src = './viz/banner.jpg',
+         alt = "Measuring Black Metal Brutality",
+         width=800)
+    
+  }, deleteFile = FALSE)
   
-   
+  output$font <- renderUI({
+    HTML("<div class='XXIIBlackmetalWarrior'><h3>Hornedeltat.</h3></div>")
+  })
+  
+  output$brutality.coefs <- renderTable({
+    df <- coefs
+    df$coef <- as.character(df$coef)
+    df[is.na(df$coef),]$coef <- 'Inf.'
+    names(df) <- c('index', 'file.name', 'brutality.coefficient')
+    select(df, file.name, brutality.coefficient)
+  })
+  
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,2 +1,15 @@
 library(shiny)
 source('helper.R')
+
+shinyUI(fluidPage(theme='bootstrap.css',
+  
+  imageOutput('banner'),
+  
+  mainPanel(
+      br(),br(),br(),br(),br(),br(),br(),br(),
+      HTML("<h2 class='XXIIBlackmetalWarrior'>A Satanic Time Series Study of Variance</h2>"),
+      HTML("Here is a <span class='XXIIBlackmetalWarrior'>pole cat</span> made of glass."),
+      tableOutput('brutality.coefs')
+  )
+  )
+)
